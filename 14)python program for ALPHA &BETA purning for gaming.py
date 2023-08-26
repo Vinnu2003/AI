a162bed7ed5af1c4bd1ef5bd66c75ee68b007d34
Vinnu2003 <114645132+Vinnu2003@users.noreply.github.com>
2023-08-26 10:35:53 +0530

    Update 14)python program for ALPHA &BETA purning for gaming.py

--- 14)python program for ALPHA &BETA purning for gaming.py
+++ 14)python program for ALPHA &BETA purning for gaming.py
@@ -1,47 +1,73 @@
 print("Vinay Kumar-192111381")
-MAX, MIN = 1000, -1000
+// C++ program to demonstrate
+// working of Alpha-Beta Pruning
+#include<bits/stdc++.h>
+using namespace std;
 
+// Initial values of
+// Alpha and Beta
+const int MAX = 1000;
+const int MIN = -1000;
 
-def minimax(depth, nodeIndex, maximizingPlayer,
-			values, alpha, beta):
-
-	if depth == 3:
-		return values[nodeIndex]
-
-	if maximizingPlayer:
+// Returns optimal value for
+// current player(Initially called
+// for root and maximizer)
+int minimax(int depth, int nodeIndex,
+			bool maximizingPlayer,
+			int values[], int alpha,
+			int beta)
+{
 	
-		best = MIN
+	// Terminating condition. i.e
+	// leaf node is reached
+	if (depth == 3)
+		return values[nodeIndex];
 
-		
-		for i in range(0, 2):
+	if (maximizingPlayer)
+	{
+		int best = MIN;
+
+		// Recur for left and
+		// right children
+		for (int i = 0; i < 2; i++)
+		{
 			
-			val = minimax(depth + 1, nodeIndex * 2 + i,
-						False, values, alpha, beta)
-			best = max(best, val)
-			alpha = max(alpha, best)
+			int val = minimax(depth + 1, nodeIndex * 2 + i,
+							false, values, alpha, beta);
+			best = max(best, val);
+			alpha = max(alpha, best);
 
+			// Alpha Beta Pruning
+			if (beta <= alpha)
+				break;
+		}
+		return best;
+	}
+	else
+	{
+		int best = MAX;
 
-			if beta <= alpha:
-				break
-		
-		return best
-	
-	else:
-		best = MAX
+		// Recur for left and
+		// right children
+		for (int i = 0; i < 2; i++)
+		{
+			int val = minimax(depth + 1, nodeIndex * 2 + i,
+							true, values, alpha, beta);
+			best = min(best, val);
+			beta = min(beta, best);
 
-		for i in range(0, 2):
-		
-			val = minimax(depth + 1, nodeIndex * 2 + i,
-							True, values, alpha, beta)
-			best = min(best, val)
-			beta = min(beta, best)
+			// Alpha Beta Pruning
+			if (beta <= alpha)
+				break;
+		}
+		return best;
+	}
+}
 
-			if beta <= alpha:
-				break
-		
-		return best
-	
-if __name__ == "__main__":
-
-	values = [3, 5, 6, 9, 1, 2, 0, -1]
-	print("The optimal value is :", minimax(0, 0, True, values, MIN, MAX))
+// Driver Code
+int main()
+{
+	int values[8] = { 3, 5, 6, 9, 1, 2, 0, -1 };
+	cout <<"The optimal value is : "<< minimax(0, 0, true, values, MIN, MAX);;
+	return 0;
+}
