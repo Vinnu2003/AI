f5a5c9ab04beac43174ce31bd54041307b69e6c0
Vinnu2003 <114645132+Vinnu2003@users.noreply.github.com>
2023-08-26 10:37:44 +0530

    Update 13)python program for MIN MAX algorithm for gaming.py

--- 13)python program for MIN MAX algorithm for gaming.py
+++ 13)python program for MIN MAX algorithm for gaming.py
@@ -1,10 +1,17 @@
 print("Vinay Kumar-192111381")
+# Python3 program to demonstrate
+# working of Alpha-Beta Pruning
+
+# Initial values of Alpha and Beta
 MAX, MIN = 1000, -1000
 
-
+# Returns optimal value for current player
+#(Initially called for root and maximizer)
 def minimax(depth, nodeIndex, maximizingPlayer,
 			values, alpha, beta):
 
+	# Terminating condition. i.e
+	# leaf node is reached
 	if depth == 3:
 		return values[nodeIndex]
 
@@ -12,7 +19,7 @@ def minimax(depth, nodeIndex, maximizingPlayer,
 	
 		best = MIN
 
-		
+		# Recur for left and right children
 		for i in range(0, 2):
 			
 			val = minimax(depth + 1, nodeIndex * 2 + i,
@@ -20,7 +27,7 @@ def minimax(depth, nodeIndex, maximizingPlayer,
 			best = max(best, val)
 			alpha = max(alpha, best)
 
-
+			# Alpha Beta Pruning
 			if beta <= alpha:
 				break
 		
@@ -29,6 +36,8 @@ def minimax(depth, nodeIndex, maximizingPlayer,
 	else:
 		best = MAX
 
+		# Recur for left and
+		# right children
 		for i in range(0, 2):
 		
 			val = minimax(depth + 1, nodeIndex * 2 + i,
@@ -36,11 +45,13 @@ def minimax(depth, nodeIndex, maximizingPlayer,
 			best = min(best, val)
 			beta = min(beta, best)
 
+			# Alpha Beta Pruning
 			if beta <= alpha:
 				break
 		
 		return best
 	
+# Driver Code
 if __name__ == "__main__":
 
 	values = [3, 5, 6, 9, 1, 2, 0, -1]
